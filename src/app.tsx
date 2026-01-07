302594e67fe2d7f232cff79c2982f176cbd7e9fb
Ivan Allen Macabontoc <ivan_dev@my.pisopay.com.ph>
2026-01-07 13:57:55 +0800

    Added Blog

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import Lenis from "@studio-freight/lenis";
 import { useEffect } from "react";
 import AppRouter from "@/routes.tsx";
-
 const App = () => {
     useEffect(() => {
         const lenis = new Lenis({
